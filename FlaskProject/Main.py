890728e7b8ba33b1d47a1d29cb1e5c3b6f0f8756
ZH <killerwhokillu@gmail.com>
2017-12-12 10:01:09 +0800

    Chan

--- FlaskProject/Main.py
+++ FlaskProject/Main.py
@@ -5,37 +5,17 @@ app = Flask(__name__)
 def login():
     return render_template('login.html')
 
-@app.route('/index')
-def index():
-    return render_template('index.html' )
-
 @app.route('/charts')
 def charts():
     return render_template('charts.html' )
 
-@app.route('/tables')
+@app.route('/listofrideP')
 def tables():
-    return render_template('tables.html' )
+    return render_template('listofridesP.html' )
 
 @app.route('/ridedetails')
 def ridedetails():
     return render_template('ridedetails.html' )
-
-@app.route('/navbar')
-def navbar():
-    return render_template('navbar.html' )
-
-@app.route('/forgot-password')
-def forgot_password():
-    return render_template('forgot-password.html' )
-
-@app.route('/cards')
-def cards():
-    return render_template('cards.html' )
-
-@app.route('/blank/')
-def blank():
-    return render_template('blank.html' )
 
 @app.route('/register')
 def register():
