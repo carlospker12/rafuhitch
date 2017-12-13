209a12c96b3bcecbe8c00352b64f017dc596ce68
carlospker12 <172714L@mymail.nyp.edu.sg>
2017-12-13 10:23:15 +0800

    committing python files

---
+++ Main.py
@@ -0,0 +1,102 @@
+from flask import Flask, render_template, request, flash, redirect, url_for, session
+from wtforms import Form, StringField, TextAreaField, RadioField, SelectField, validators, PasswordField, form
+from createride import Createdriverride as Createdriverride
+import firebase_admin
+from firebase_admin import credentials, db
+
+cred = credentials.Certificate('./cred/rafuhitch-firebase-adminsdk-ip26u-288aa3dbc4.json')
+default_app = firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://rafuhitch.firebaseio.com/ '
+})
+
+root = db.reference()
+
+
+
+app = Flask(__name__)
+
+
+class createdriverrideform(Form):
+    from_where = StringField('From')
+    to_where = StringField('To')
+    date = StringField('Date')
+    time = StringField('Time')
+    type = RadioField('Type Of Publication', choices=[('driver')], default='type')
+
+
+@app.route('/')
+def start():
+    return render_template('login.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/createridedriver',methods=["GET","POST"])
+def new():
+    form = createdriverrideform(request.form)
+    if request.method == 'POST' and form.validate():
+        if  form.type.data == 'driver':
+            from_where= form.from_where.data
+            to_where = form.to_where.data
+            date = form.date.data
+            time = form.time.data
+
+            cdr = Createdriverride(from_where,to_where,date,time)
+
+            cdr_db = root.child('listofrides')
+            cdr_db.push({
+                    'type': cdr.get_type(),
+                    'from': cdr.get_from_where(),
+                    'to': cdr.get_to(),
+                    'date': cdr.get_date(),
+                    'time': cdr.get_time(),
+
+            })
+
+            flash('Magazine Inserted Sucessfully.', 'success')
+
+
+
+        return redirect(url_for('listofridesp'))
+
+
+    return render_template('create_ride_driver.html', form= form)
+
+
+
+@app.route('/listofridesp')
+def tables():
+    listofridesp = root.child('listofridesp').get()
+    list = []
+    for id in listofridesp:
+
+        eachupdate = listofridesp[id]
+
+        if eachupdate['type'] == 'driver':
+            createride = Createdriverride( eachupdate['from'], eachupdate['to'],
+                                eachupdate['date'], eachupdate['time'])
+            createride.set_type(type)
+            print(createride.get_type())
+            list.append(createride)
+    return render_template('listofridesP.html' )
+
+@app.route('/ridedetails')
+def ridedetails():
+    return render_template('ridedetails.html' )
+
+@app.route('/register')
+def register():
+    return render_template('register.html' )
+
+@app.route('/driverprofile')
+def driver_profile():
+    return render_template('Driver_Profile.html' )
+
+@app.route('/registerdriver')
+def register_driver():
+    return render_template('register_driver.html' )
+
+if __name__ == "__main__":
+    app.secret_key = 'secret123'
+    app.run(debug=True)
---
+++ createride.py
@@ -0,0 +1,37 @@
+class Createdriverride:
+    def __init__(self, type,from_where,to,date,time):
+        self.__from_where = from_where
+        self.__to = to
+        self.__type = type
+        self.__date = date
+        self.__time = time
+
+    def get_from_where(self):
+        return self.__from_where
+
+    def set_from_where(self, from_where):
+        self.__from_where = from_where
+
+    def get_type(self):
+        return self.__type
+
+    def set_type(self, type):
+        self.__type = type
+
+    def get_to(self):
+        return self.__to
+
+    def set_to(self, to):
+        self.__from_to = to
+
+    def get_date(self):
+        return self.__date
+
+    def set_date(self, date):
+        self.__date = date
+
+    def get_time(self):
+        return self.__time
+
+    def set_time(self, time):
+        self.__time = time
