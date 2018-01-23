ecb8e0a25145d789fe9d2f6592b9cb61b4ff2c60
carlospker12 <172714L@mymail.nyp.edu.sg>
2018-01-23 10:08:57 +0800

    stg

--- signup.py
+++ signup.py
@@ -1,11 +1,14 @@
 class User:
-    def __init__(self,name,email,password):
+    def __init__(self,name,email,password,username):
         self.__name=name
+        self.__username=username
         self.__email=email
         self.__password=password
 
     def set_name(self,name):
         self._name=name
+    def set_username(self,username):
+        self._username=username
     def set_email(self,email):
         self.__email=email
     def set_password(self,password):
@@ -13,6 +16,8 @@ class User:
 
     def get_name(self):
         return self.__name
+    def get_username(self):
+        return self.__username
     def get_email(self):
         return self.__email
     def get_password(self):
